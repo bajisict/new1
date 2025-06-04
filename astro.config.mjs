3e8a61f73ce8362edd4a85b767d8db77ed7267f0
bajisict <vcad.nm@gmail.com>
2025-06-04 13:26:44 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-// import db from '@astrojs/db';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone' // or "middleware" if you're deploying to something like Netlify Functions
-  }),
+  integrations: [react()]
 });
