17633503d7f2a04f89680399640de6f02054a85c
bajisict <vcad.nm@gmail.com>
2025-06-04 13:11:29 +0800

    fix

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_ig-GBzj6.mjs';
-import { manifest } from './manifest_kRpl3mIG.mjs';
+import { manifest } from './manifest_wbT5V1WA.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f8d24814-6bdd-4f18-9376-487b980d0c04",
+    "middlewareSecret": "98dbefb4-ef83-44d3-aff2-f8660932dc0c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
